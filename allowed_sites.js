077b7161f3ab58b4c4ada84c7e588e8b11bd62ef
Alon Tanay <49856257+alontanay@users.noreply.github.com>
2022-03-13 14:09:09 +0200

    Add files via upload

---
+++ allowed_sites.js
@@ -0,0 +1,6 @@
+const allowedSites = [
+  "https://contest.cs.biu.ac.il/home_a",
+  "https://contest.cs.biu.ac.il/home_b",
+  "https://contest.cs.biu.ac.il/home_a2021",
+  "https://contest.cs.biu.ac.il/home_a_old"
+];
